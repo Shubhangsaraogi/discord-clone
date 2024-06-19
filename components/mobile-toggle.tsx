dc20312c36d46752c5a977bb729bb99a190c4f90
Shubhang <s.saraogi1902@gmail.com>
2024-06-20 02:23:56 +0530

    final commit

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -16,17 +16,17 @@ export const MobileToggle = ({
 }) => {
   return (
     <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu />
-        </Button>
-      </SheetTrigger>
       <SheetContent side="left" className="p-0 flex gap-0">
         <div className="w-[72px]">
           <NavigationSidebar />
         </div>
         <ServerSidebar serverId={serverId} />
       </SheetContent>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
     </Sheet>
   )
 }
